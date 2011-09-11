619c8b62df9586d93ca541627ed90eb89b400658
Chris McKenzie <kristopolous@yahoo.com>
2011-09-11 16:05:23 -0700

    code refactoring

---
+++ js/related.js
@@ -0,0 +1,47 @@
+
+function sort(mode, el) {
+	sortOrder = mode;
+
+	$(el)
+    .addClass('selected')
+    .siblings()
+    .removeClass('selected');
+
+	gen();
+}
+
+function loadit(ytid){
+  ev.isset('flash.load', function(){
+    if(!db.findFirst({ytid: ytid}).serverData) {
+
+      var id = Timeline.add(ytid);
+
+      $.getJSON(
+        'api/related.php',
+        {v: ytid}, 
+
+        function (data){
+          db.insert({
+            ytid: ytid
+          }).update({
+            removed: 0,
+            count: 0,
+            length: data.length,
+            title: Utils.clean(data.title),
+            related: _.map(data.related, function(which) {
+              return which[1];
+            }),
+            serverData: data
+          });
+
+          addVids(data.related);
+      
+          Timeline.update(id);
+
+          gen();
+        });
+    } else {
+      Timeline.add(ytid);
+    }
+  });
+}
---
+++ js/search.js
@@ -0,0 +1,90 @@
+$(function(){
+  var 
+    lastSearch = '', 
+    searchID = 0, 
+    lastID = 0;
+
+  Utils.onEnter(".search", function(){
+    search(this.value);
+  });
+
+  $("#normal-search").focus(function(){
+    this.select();
+  });
+
+  setInterval(function(){
+    var query = $("#normal-search").val();
+
+    if(query != lastSearch) {
+      lastSearch = query;
+
+      $.getJSON('api/ytsearch.php', { 
+          id: ++searchID,
+          q: query
+        }, function(res) {
+        console.log(res);
+
+        if(res.id < lastID) {
+          return;
+        }
+
+        lastID = res.id;
+
+        var instance;
+
+        $("#search-results").children().remove();
+
+        for(var ix = 0; ix < res.vidList.length; ix++) {
+          instance = res.vidList[ix];
+
+          addVideo(_.extend(
+            {container: "#search-results"},
+            instance
+          ));
+        }
+      });
+    }
+  }, 650);
+});
+
+function search(query) {
+  transition();
+
+  Local.create();
+
+  if(query.slice(0,5).toLowerCase() == 'http:') {
+    var parts = query.split(/[\.=&#?]/);
+
+    for(var ix = 0, len = parts.length; ix < len; ix++){
+
+      if(parts[ix] == 'v') {
+
+        loadit(parts[ix + 1]);
+        return;
+      }
+    }
+  } else {
+    $("#normal-search").val(query);
+    return;
+  }
+
+	if(query.length == 0) {
+		query = false;
+	  gen();
+  } else {
+    if(!serverSearched[query]) {
+      serverSearched[query] = true;
+
+      $.getJSON('api/yt_search.php',
+        {q: query}, 
+        function(data) {
+          serverSearched[query] = _.map(data, function(which) {
+            return which[1];
+          });
+
+          addVids(data);
+          gen();
+        });
+    }
+  }
+}
