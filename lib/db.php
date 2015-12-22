71a9b49393b9c58e1800c7a3e87667a79f747573
Chris McKenzie <kristopolous@yahoo.com>
2015-12-22 13:07:12 -0800

    erroring on db failure

--- lib/db.php
+++ lib/db.php
@@ -1,6 +1,13 @@
 <?php
 session_start();
-$db = mysql_connect('localhost', 'root', '');
+$db_host = 'localhost';
+$db = @mysql_connect($db_host, 'root', '');
+
+if(!$db) {
+  echo "Unable to connect to the server $db_host. Are you sure it's running?";
+  exit(0);
+}
+
 mysql_select_db('yt', $db);
 //date_default_timezone_set("America/Los_Angeles");
 //define("YT-DEVKEY", "AI39si5NzWIiUYbOU5apApD9fKp2DsAcIN094TqmfjnQitcv8Fkc-wvJePeDP2IP0E11UJPZohD4dbSkyQnRBSqgVZC314tR_w");
