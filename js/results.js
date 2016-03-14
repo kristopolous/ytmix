1efa2b706194097f55203ca6ba7ad03fb5b052f8
Chris McKenzie <kristopolous@yahoo.com>
2016-03-14 16:10:04 -0700

    using colons for parsing

--- js/results.js
+++ js/results.js
@@ -102,7 +102,7 @@ var Results = {
         splitup = splitup[0].split(/ -+/);
       }
       if(splitup.length == 1) {
-        splitup = splitup[0].split(/ @ /);
+        splitup = splitup[0].split(/ [@:]* /);
       }
       if(splitup.length == 1) {
         splitup = splitup[0].split(/-/);
