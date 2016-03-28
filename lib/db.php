cc9e08605659d7a9686ed0e2f3c876190d719a49
Chris McKenzie <kristopolous@yahoo.com>
2016-03-28 16:29:11 -0700

    sourcing creds from centralized ini file

--- lib/db.php
+++ lib/db.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-$db_host = 'localhost';
-$db = @mysql_connect($db_host, 'root', '');
+$db_params = parse_ini_file('../secrets/db.ini');
+$db_host = $db_params['host'];
+$db = @mysql_connect($db_host, $db_params['user'], $db_params['password']);
 
 if(!$db) {
   echo "Unable to connect to the server $db_host. Are you sure it's running?";
@@ -9,5 +10,3 @@ if(!$db) {
 }
 
 mysql_select_db('yt', $db);
-//date_default_timezone_set("America/Los_Angeles");
-//define("YT-DEVKEY", "AI39si5NzWIiUYbOU5apApD9fKp2DsAcIN094TqmfjnQitcv8Fkc-wvJePeDP2IP0E11UJPZohD4dbSkyQnRBSqgVZC314tR_w");
