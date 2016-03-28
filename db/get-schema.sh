4b67393f5124d2da61604758abe95eb123498336
Chris McKenzie <kristopolous@yahoo.com>
2016-03-28 16:27:05 -0700

    Sourcing the credentials from db.ini

--- db/get-schema.sh
+++ db/get-schema.sh
@@ -1,1 +1,8 @@
-mysqldump -u root --no-data yt > schema  
+#!/bin/sh
+. ../secrets/db.ini
+
+if [ -z "$password" ]; then
+  mysqldump -u root --no-data yt > schema  
+else 
+  mysqldump -u $user --no-data -p$password yt > schema
+fi
