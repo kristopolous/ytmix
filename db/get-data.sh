11a30ea1dde9eda87bfdb4a1ad180ef4ad383f62
Chris McKenzie <kristopolous@yahoo.com>
2016-03-28 16:27:19 -0700

    adding hygenic whitespace

--- db/get-data.sh
+++ db/get-data.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 . ../secrets/db.ini
+
 echo "Dumping"
 if [ -z "$password" ]; then
   mysqldump -u $user yt > mysql-dump.db
 else 
   mysqldump -u $user -p$password yt > mysql-dump.db
 fi
+
 echo "Compressing"
 [ -e mysql-dump.db.lzma ] && rm mysql-dump.db.lzma
 lzma -9 mysql-dump.db
