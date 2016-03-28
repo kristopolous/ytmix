8394740d58785e4d02694a4886dcdcf6757cfc6b
Chris McKenzie <kristopolous@yahoo.com>
2016-03-28 16:22:25 -0700

    Using centralized database credential file

--- db/get-data.sh
+++ db/get-data.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
+. ../secrets/db.ini
 echo "Dumping"
-mysqldump -u root yt > mysql-dump.db
+if [ -z "$password" ]; then
+  mysqldump -u $user yt > mysql-dump.db
+else 
+  mysqldump -u $user -p$password yt > mysql-dump.db
+fi
 echo "Compressing"
 [ -e mysql-dump.db.lzma ] && rm mysql-dump.db.lzma
 lzma -9 mysql-dump.db
