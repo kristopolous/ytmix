daa782bf6847d9c978f36165991a2c81020b82bf
Chris McKenzie <kristopolous@yahoo.com>
2016-04-06 11:10:07 -0700

    fixing player issues

--- js/timeline.js
+++ js/timeline.js
@@ -653,7 +653,7 @@ var Timeline = (function(){
 
           swfobject.embedSWF("http://www.youtube.com/apiplayer?" +
             "version=3&enablejsapi=1&playerapiid=player-" + ix,
-            "vidContainer-" + ix, "80", "60", "9", null, null, 
+            "vidContainer-" + ix, "300", "200", "9", null, null, 
             {allowScriptAccess: "always"}, {id: 'player-' + ix});
         }
       } else {
