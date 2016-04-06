36f67396216bd2f2899e867dcdb2b6ad1612d426
Chris McKenzie <kristopolous@yahoo.com>
2016-04-06 11:47:07 -0700

    using mysqli

--- lib/db.php
+++ lib/db.php
@@ -2,11 +2,9 @@
 session_start();
 $db_params = parse_ini_file('../secrets/db.ini');
 $db_host = $db_params['host'];
-$db = @mysql_connect($db_host, $db_params['user'], $db_params['password']);
+$db = @mysqli_connect($db_host, $db_params['user'], $db_params['password'], 'yt');
 
 if(!$db) {
   echo "Unable to connect to the server $db_host. Are you sure it's running?";
   exit(0);
 }
-
-mysql_select_db('yt', $db);
