13d9ed563bfe4246a1ef64661c2f765767ad56ec
Chris McKenzie <kristopolous@yahoo.com>
2016-06-17 17:12:12 -0700

    making sure that errors don't make the mysql_connect explode

--- lib/db.php
+++ lib/db.php
@@ -7,7 +7,7 @@ function get_db() {
   if(!$_db) {
     $db_params = parse_ini_file('../secrets/db.ini');
     $db_host = $db_params['host'];
-    $_db = mysqli_connect($db_host, $db_params['user'], $db_params['password'], 'yt');
+    $_db = @mysqli_connect($db_host, $db_params['user'], $db_params['password'], 'yt');
 
     if(!$_db) {
       echo "Unable to connect to the server $db_host. Are you sure it's running?";
